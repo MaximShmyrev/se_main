9984276aa5bf7618d596668299e138b599a7ca81
MaximShmyrev <maxim.shmyrev@gmail.com>
2021-03-07 23:56:43 +0300

    quiz app: analytics view

--- controller/Analytics.controller.js
+++ controller/Analytics.controller.js
@@ -1,82 +1,294 @@
-sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', 'sap/viz/ui5/data/FlattenedDataset',
-		'sap/viz/ui5/controls/common/feeds/FeedItem', 'sap/m/Label',
-		'sap/m/ColumnListItem', 'sap/m/library', 'sap/m/Column', 'sap/viz/ui5/controls/common/feeds/AnalysisObject',
-		'sap/viz/ui5/controls/VizFrame', 'sap/m/Table'
-	],
-	function(Controller, JSONModel, FlattenedDataset, FeedItem, Label, ColumnListItem, MobileLibrary, Column,
-		AnalysisObject, VizFrame, Table) {
+sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', 'sap/viz/ui5/data/FlattenedDataset', 'sap/viz/ui5/controls/common/feeds/FeedItem', 'sap/m/Label',
+	'sap/m/ColumnListItem', 'sap/m/library', 'sap/m/MessageToast', 'sap/m/Column'
+],
+	function (Controller, JSONModel, FlattenedDataset, FeedItem, Label, ColumnListItem, MobileLibrary, MessageToast, Column) {
 		"use strict";
 
-		var oData = {
-			"items": [{
-				"Date": "08/01/2021",
-				"Result": "20"
-			}, {
-				"Date": "09/01/2021",
-				"Result": "50"
-			}]
+		var json;
+		var oData;
+		var results;
+
+		//request data from backend
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open('POST', 'http://prt.samaraenergo.ru:50000/ZCE_TestsService/ZCE_Tests', false);
+
+		// build SOAP request
+		var sr = '<?xml version="1.0" encoding="utf-8"?>' +
+			'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:zce="http://samaraenergo.ru/zce_tests/">' +
+			'<soapenv:Header/>' +
+			'<soapenv:Body>' +
+			'<zce:getAnalytic/>' +
+			'</soapenv:Body>' +
+			'</soapenv:Envelope>';
+
+
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState == 4) {
+				if (xmlhttp.status == 200) {
+					//parse response and create array for quizes
+					var response = xmlhttp.responseText;
+					var splitResponse = response.split(/<return>|<\/return>/);
+					var arrayResponse = splitResponse[1];
+					results = JSON.parse(arrayResponse.toString());
+
+					//add state property to oData
+					for (var i = 0; i < results.ANALYTICS.length; i++) {
+						for (var j = 0; j < results.ANALYTICS[i].results.length; j++) {
+
+							var isAnswer;
+
+							if (ANALYTICS[i].results[j].ISANSWER == "1") {
+								isAnswer = "верно"
+							} else {
+								isAnswer = "неверно"
+							};
+
+							oData = {
+								user: results.ANALYTICS[i].USERID,
+								date: results.ANALYTICS[i].DATETIME,
+								test: results.ANALYTICS[i].results[j].DESCRIPTION,
+								question: ANALYTICS[i].results[j].QUESTIONTEXT,
+								answer: ANALYTICS[i].results[j].ANSWERTEXT,
+								result: currentQuestion,
+							};
+						}
+					}
+
+					// var index = 0;
+					// oData = {
+					// 	user: results[i].USERID,
+					// 	date: results[i].DATETIME
+						//test: results[i].results.DESCRIPTION,
+						//questtion: currentQuestion,
+						//answer: sessionStorage.getItem("QUIZTITLE"),
+						//result: currentQuestion,
+					};
+
+				}
+			}
 		};
+
+		// Send the POST request
+		xmlhttp.setRequestHeader("Content-Type", "text/xml");
+		xmlhttp.send(sr);
 
 		var oPageController = Controller.extend("main.controller.Analytics", {
 
-			onInit: function() {
+			/* ============================================================ */
+			/* Constants                                                    */
+			/* ============================================================ */
 
-				var sampleDatajson = new sap.ui.model.json.JSONModel(oData);
-				var oVizFrame = this.getView().byId("idStackedChart");
-				oVizFrame.setVizProperties({
-					plotArea: {
-						colorPalette: d3.scale.category20().range(),
-						dataLabel: {
-							showTotal: true
+			_constants: {
+				sampleName: "main",
+				chartContainerId: "idChartContainer",
+				vizFrame: {
+					id: "idoVizFrame",
+					modulePath: "./ChartContainerData1.json",
+					dataset: {
+						dimensions: [{
+							name: 'Country',
+							value: "{Country}"
+						}],
+						measures: [{
+							group: 1,
+							name: "Profit",
+							value: "{Profit}"
+						}, {
+							group: 1,
+							name: "Target",
+							value: "{Target}"
+						}, {
+							group: 1,
+							name: "Forecast",
+							value: "{Forecast}"
+						}, {
+							group: 1,
+							name: "Revenue",
+							value: "{Revenue}"
+						}, {
+							group: 1,
+							name: "Revenue2",
+							value: "{Revenue2}"
+						}, {
+							group: 1,
+							name: "Revenue3",
+							value: "{Revenue3}"
+						}],
+						data: {
+							path: "/Products"
 						}
 					},
-					tooltip: {
-						visible: true
-					},
-					title: {
-						text: "Cводный график"
-					}
-				});
-				var oDataset = new sap.viz.ui5.data.FlattenedDataset({
-					dimensions: [{
-						name: "Дата",
-						value: "{Date}"
-					}],
-					measures: [{
-						name: "Результат",
-						value: "{Result}"
-					}],
-
-					data: {
-						path: "/items"
-					}
-				});
-				oVizFrame.setDataset(oDataset);
-
-				oVizFrame.setModel(sampleDatajson);
-
-				var oFeedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
-						"uid": "valueAxis",
+					type: "line",
+					feedItems: [{
+						"uid": "primaryValues",
 						"type": "Measure",
-						"values": ["Результат"]
-					}),
-
-					oFeedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
-						"uid": "categoryAxis",
+						"values": ["Revenue"]
+					}, {
+						"uid": "axisLabels",
 						"type": "Dimension",
-						"values": ["Дата"]
-					});
-
-				oVizFrame.addFeed(oFeedValueAxis);
-				oVizFrame.addFeed(oFeedCategoryAxis);
-
+						"values": ["Country"]
+					}, {
+						"uid": "targetValues",
+						"type": "Measure",
+						"values": ["Target"]
+					}]
+				},
+				table: {
+					modulePath: "./ChartContainerData2.json",
+					itemBindingPath: "/businessData",
+					columnLabelTexts: ["Пользователь", "Дата", "Анкета", "Вопрос", "Ответ", "Результат"],
+					templateCellLabelTexts: ["{Sales_Month}", "{Marital Status}", "{Customer Gender}", "{Sales_Quarter}", "{Cost}", "{Unit Price}"]
+				}
 			},
 
-			onNavBack: function() {
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("quizmain");
-			}
+			/* ============================================================ */
+			/* Life-cycle Handling                                          */
+			/* ============================================================ */
 
+			onInit: function () {
+
+				json = {
+					"businessData": [{
+						"Sales_Month": "April",
+						"Marital Status": "Married",
+						"Customer Gender": "Female",
+						"Sales_Quarter": "Q1",
+						"Cost": 190,
+						"Unit Price": 328.3,
+					},
+					{
+						"Sales_Month": "May",
+						"Marital Status": "Married",
+						"Customer Gender": "Female",
+						"Sales_Quarter": "Q2",
+						"Cost": 189.9,
+						"Unit Price": 151.17,
+					}
+					]
+				};
+
+
+				// create table content
+				var oTable = this.getView().byId("idTable");
+				this._createTableContent(oTable);
+
+				//create chart content
+				var oVizFrame = this.getView().byId(this._constants.vizFrame.id);
+				this._updateVizFrame(oVizFrame);
+			},
+
+			/* ============================================================ */
+			/* Helper Methods                                               */
+			/* ============================================================ */
+			/**
+			 * Creates table content for the chart container
+			 * @param {sap.m.Table} oTable view table
+			 * @private
+			 */
+			_createTableContent: function (oTable) {
+				var oTablePath = jQuery.sap.getModulePath(this._constants.sampleName, this._constants.table.modulePath);
+				//var oTableModel = new JSONModel(oTablePath);
+				var oTableModel = new JSONModel(json);
+				var oTableConfig = this._constants.table;
+				var aColumns = this._createTableColumns(oTableConfig.columnLabelTexts);
+
+				for (var i = 0; i < aColumns.length; i++) {
+					oTable.addColumn(aColumns[i]);
+				}
+
+				var oTableItemTemplate = new ColumnListItem({
+					type: MobileLibrary.ListType.Active,
+					cells: this._createLabels(oTableConfig.templateCellLabelTexts)
+				});
+
+				oTable.bindItems(oTableConfig.itemBindingPath, oTableItemTemplate, null, null);
+				oTable.setModel(oTableModel);
+			},
+			/**
+			 * Calls the message toast show method with the given message.
+			 *
+			 * @private
+			 * @param {String} message Message for message toast
+			 */
+			_showMessageToast: function (message) {
+				MessageToast.show(message);
+			},
+			/**
+			 * Updates the Viz Frame with the necessary data and properties.
+			 *
+			 * @private
+			 * @param {sap.viz.ui5.controls.VizFrame} vizFrame Viz Frame to update
+			 */
+			_updateVizFrame: function (vizFrame) {
+				var oVizFrame = this._constants.vizFrame;
+				var oDataset = new FlattenedDataset(this._constants.vizFrame.dataset);
+				var oVizFramePath = jQuery.sap.getModulePath(this._constants.sampleName, oVizFrame.modulePath);
+				var oModel = new JSONModel(oVizFramePath);
+
+				vizFrame.setDataset(oDataset);
+				vizFrame.setModel(oModel);
+				this._addFeedItems(vizFrame, oVizFrame.feedItems);
+				vizFrame.setVizType(oVizFrame.type);
+			},
+			/**
+			 * Adds the passed feed items to the passed Viz Frame.
+			 *
+			 * @private
+			 * @param {sap.viz.ui5.controls.VizFrame} vizFrame Viz Frame to add feed items to
+			 * @param {Object[]} feedItems Feed items to add
+			 */
+			_addFeedItems: function (vizFrame, feedItems) {
+				for (var i = 0; i < feedItems.length; i++) {
+					vizFrame.addFeed(new FeedItem(feedItems[i]));
+				}
+			},
+			/**
+			 * Creates table columns with labels as headers.
+			 *
+			 * @private
+			 * @param {String[]} labels Column labels
+			 * @returns {sap.m.Column[]} Array of columns
+			 */
+			_createTableColumns: function (labels) {
+				var aLabels = this._createLabels(labels);
+				return this._createControls(Column, "header", aLabels);
+			},
+			/**
+			 * Creates label control array with the specified texts.
+			 *
+			 * @private
+			 * @param {String[]} labelTexts text array
+			 * @returns {sap.m.Column[]} Array of columns
+			 */
+			_createLabels: function (labelTexts) {
+				return this._createControls(Label, "text", labelTexts);
+			},
+			/**
+			 * Creates an array of controls with the specified control type, property name and value.
+			 *
+			 * @private
+			 * @param {sap.ui.core.Control} Control Control type to create
+			 * @param {String} prop Property name
+			 * @param {Array} propValues Value of the control's property
+			 * @returns {sap.ui.core.Control[]} array of the new controls
+			 */
+			_createControls: function (Control, prop, propValues) {
+				var aControls = [];
+				var oProps = {};
+				for (var i = 0; i < propValues.length; i++) {
+					oProps[prop] = propValues[i];
+					aControls.push(new Control(oProps));
+				}
+				return aControls;
+			},
+			/**
+			 * Creates message for a press event on custom button.
+			 * @param {sap.ui.base.Event} event the press event object
+			 * @private
+			 */
+			onCustomActionPress: function (event) {
+				this._showMessageToast("Custom action press event - " + event.getSource().getId());
+			}
 		});
 		return oPageController;
 	});
