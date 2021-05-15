c4ccd17212e602879bde63ddf5b3df1338af3f54
MaximShmyrev <maxim.shmyrev@gmail.com>
2021-05-15 15:13:25 +0300

    news admin app: layout creation

--- controller/NewsAdmin.controller.js
+++ controller/NewsAdmin.controller.js
@@ -1,18 +1,51 @@
 sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
 	"sap/base/Log"
-], function (MessageToast, Controller, Log) {
+], function (MessageToast, Controller, JSONModel, Log) {
 	"use strict";
+
+	var news;
+	var newsJson;
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('POST', 'http://prt.samaraenergo.ru:50000/ZCE_NewsService/ZCE_News', false);
+
+	// build SOAP request
+	var sr = '<?xml version="1.0" encoding="utf-8"?>' +
+		'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:zce="http://samaraenergo.ru/zce_news/">' +
+		'<soapenv:Header/>' +
+		'<soapenv:Body>' +
+		'<zce:getNews/>' +
+		'</soapenv:Body>' +
+		'</soapenv:Envelope>';
+
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4) {
+			if (xmlhttp.status == 200) {
+
+				var response = xmlhttp.responseText;
+
+				var splitResponse = response.split(/<return>|<\/return>/);
+				var arrayResponse = splitResponse[1];
+				newsJson = JSON.parse(arrayResponse.toString());
+			}
+		}
+	}
+
+
+	// Send the POST request
+	xmlhttp.setRequestHeader("Content-Type", "text/xml");
+	xmlhttp.send(sr);
 
 	return Controller.extend("main.controller.NewsAdmin", {
 
 		onInit: function () {
-			this.getSplitAppObj().setHomeIcon({
-				'phone': 'phone-icon.png',
-				'tablet': 'tablet-icon.png',
-				'icon': 'desktop.ico'
-			});
+
+			var oModel = new JSONModel(newsJson);
+			this.getView().setModel(oModel);
+
 		},
 
 		onOrientationChange: function (oEvent) {
