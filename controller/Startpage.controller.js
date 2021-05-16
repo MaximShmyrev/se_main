1b582119b574164bb17b212582590dd17ca0c1db
alexteslya <alex.teslja@gmail.com>
2021-05-16 16:38:46 +0300

    limit number of news for first page to 10

--- controller/Startpage.controller.js
+++ controller/Startpage.controller.js
@@ -75,7 +75,7 @@ sap.ui.define([
 						var splitResponse = response.split(/<return>|<\/return>/);
 						var arrayResponse = splitResponse[1];
 						jsonObj = JSON.parse(arrayResponse.toString());
-
+						jsonObj.splice(10);
 					}
 				}
 			};
